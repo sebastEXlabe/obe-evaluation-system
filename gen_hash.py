929ba8505a0ee786ec19277871c20f6fbb597e6c
sebastEXlabe <sebasttt@163.com>
2026-07-07 06:03:18 +0800

    chore: 彻底清理所有非核心文件(论文/文档/测试)

--- gen_hash.py
+++
@@ -1,7 +0,0 @@
-import hashlib, secrets, base64
-password = 'Admin123!'
-salt = secrets.token_hex(12)
-iterations = 1000000
-dk = hashlib.pbkdf2_hmac('sha256', password.encode(), salt.encode(), iterations)
-hash_val = base64.b64encode(dk).decode().strip()
-print(f'pbkdf2_sha256${iterations}${salt}${hash_val}')
